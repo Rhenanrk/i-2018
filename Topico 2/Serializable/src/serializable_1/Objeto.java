cc0b31161c03b72c0aa65ba0c0d05c1a1afcfc21
Rhenanrk <rhenan_Konrad@hotmail.com>
2018-05-08 17:34:12 -0300

    Acrécimo de documentação

---
+++ Topico 2/Serializable/src/serializable_1/Objeto.java
@@ -0,0 +1,26 @@
+package serializable_1;
+
+import java.io.Serializable;
+
+public class Objeto {
+
+    private String nome;
+
+    public Objeto(String nome) {
+
+        super();
+
+        this.nome = nome;
+    }
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+}
