f63a2c46ecd4010a041ed429f328d37790add063
Rhenanrk <rhenan_Konrad@hotmail.com>
2018-05-30 23:56:14 -0300

    Criação da conexão com o banco de dados

---
+++ relacional/src/main/java/br/com/rhenanrk/integracao/ConexaoUtil.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018.
+ * Rhenan Konrad
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package br.com.rhenanrk.integracao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Classe responsável por realizar a conexão com o banco de dados
+ */
+public class ConexaoUtil {
+    String serverName = "localhost"; // caminho do servidor
+    String myDataBase = "teste"; // nome do banco de dados
+    String url = "jdbc:mysql://" + serverName + "/" + myDataBase + "?autoReconnect=true&useSSL=false"; // construção da URL
+    String username = "root"; // usuário do banco de dados
+    String password = "12345"; // senha do banco de dados
+
+    private static ConexaoUtil conexaoUtil;
+
+    /**
+     * Método responsável por testar se ja existe alguma conexão ao banco
+     */
+    public static ConexaoUtil getInstance() {
+        if (conexaoUtil == null) {
+            conexaoUtil = new ConexaoUtil();
+        }
+        return conexaoUtil;
+    }
+
+    /**
+     * Método responsável por realizar a conexão;
+     */
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
