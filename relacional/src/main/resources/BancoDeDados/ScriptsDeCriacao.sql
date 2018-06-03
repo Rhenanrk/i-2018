b0aa67eece0b3a3b501d81c0d558a715771c53c1
Rhenanrk <rhenan_Konrad@hotmail.com>
2018-06-03 03:37:23 -0300

    Atualizaçào do script de criação do Banco de dados

--- relacional/src/main/resources/BancoDeDados/ScriptsDeCriacao.sql
+++ relacional/src/main/resources/BancoDeDados/ScriptsDeCriacao.sql
@@ -7,114 +7,112 @@ CREATE DATABASE relacional;
 USE relacional;   
 
 /*Criando tabela de indivíduo*/
-create table individuo (
-surrogateKey varchar (10) primary key);
+CREATE TABLE individuo (
+surrogateKey VARCHAR (10) primary key);
 
 /*Criando tabela de nome*/
-create table nome (
-surrogateKey varchar (10),
-titulos varchar (20),
-nomes varchar (30),
-sobrenomes varchar (30),
-sufixos varchar (20),
-preferido int,
-usoCondicional int,
-uso int,
-inicioUso varchar (12),
-fimUso varchar (12));
+CREATE TABLE nome (
+surrogateKey VARCHAR (10),
+titulos VARCHAR (20),
+nomes VARCHAR (30),
+sobrenomes VARCHAR (30),
+sufixos VARCHAR (20),
+preferido INT,
+usoCondicional INT,
+uso INT,
+inicioUso VARCHAR (12),
+fimUso VARCHAR (12));
 
 /*Criando tabela de representaçao*/
-create table representacao (
-surrogateKey varchar (10),
-utilizacao varchar (30),
-alternativa varchar (30));
+CREATE TABLE representacao (
+surrogateKey VARCHAR (10),
+utilizacao VARCHAR (30),
+alternativa VARCHAR (30));
 
 /*Criando tabela de identificador*/
-create table identificador (
-surrogateKey varchar (10),
-idCod varchar (10),
-designacao varchar (25),
-area int,
-emissor varchar (25),
-dataEmissao varchar (12),
-tipo int);
+CREATE TABLE identificador (
+surrogateKey VARCHAR (10),
+idCod VARCHAR (10),
+designacao VARCHAR (25),
+area INT,
+emissor VARCHAR (25),
+dataEmissao VARCHAR (12),
+tipo INT);
 
 /*Criando tabela de titulo eleitoral*/
-create table tituloEleitoral (
-idCod varchar (10),
-surrogateKey varchar (10),
-secao int,
-zona int);
+CREATE TABLE tituloEleitoral (
+idCod VARCHAR (10),
+surrogateKey VARCHAR (10),
+secao INT,
+zona INT);
 
 /*Criando tabela de certidão*/
-create table certidao (
-idCod varchar (10),
-surrogateKey varchar (10),
-tipo int,
-nomeCartorio varchar (30),
-livro int,
-folha int,
-termo int);
+CREATE TABLE certidao (
+idCod VARCHAR (10),
+surrogateKey VARCHAR (10),
+tipo INT,
+nomeCartorio VARCHAR (30),
+livro INT,
+folha INT,
+termo INT);
 
 /*Criando tabela de carteira de trabalho e previdencia social*/
-create table ctps (
-idCod varchar (10),
-surrogateKey varchar (10),
-serie varchar (10),
-estado varchar (20));
+CREATE TABLE ctps (
+idCod VARCHAR (10),
+surrogateKey VARCHAR (10),
+serie VARCHAR (10),
+estado VARCHAR (20));
 
 /*Criando tabela de dado demográfico*/
-create table dadoDemografico (
-surrogateKey varchar (10),
-dataNascimento varchar (12),
-nascimentoAcuracia varchar (2),
-dataObito varchar (12),
-obitoAcuracia varchar (2),
-fonteNotificacaoObito int,
-sexo varchar (2),
-nomeMae varchar (40),
-nomePai varchar (40),
-situacaoFamiliar int,
-racaCor int,
-comentario varchar (100),
-nascimentoPluralidade int,
-nascimentoOrdem int,
-nascimentoSeguinte int,
-pais varchar (20),
-estado varchar (20),
-municipio varchar (40),
-dataEntradaPais varchar (12));
+CREATE TABLE dadoDemografico (
+surrogateKey VARCHAR (10),
+dataNascimento VARCHAR (12),
+nascimentoAcuracia VARCHAR (2),
+dataObito VARCHAR (12),
+obitoAcuracia VARCHAR (2),
+fonteNotificacaoObito INT,
+sexo VARCHAR (2),
+nomeMae VARCHAR (40),
+nomePai VARCHAR (40),
+situacaoFamiliar INT,
+racaCor INT,
+comentario VARCHAR (100),
+nascimentoPluralidade INT,
+nascimentoOrdem INT,
+nascimentoSeguINTe INT,
+pais VARCHAR (20),
+estado VARCHAR (20),
+municipio VARCHAR (40),
+dataEntradaPais VARCHAR (12));
 
 /*Criando tabela de endereço*/
-create table endereco (
-surrogateKey varchar (10),
-tipo int,
-endereco varchar (40),
-bairro varchar (30),
-distrito varchar (30),
-municipio varchar (30),
-estado varchar (20),
-cep varchar (10),
-caixaPostal varchar (15),
-pais varchar (30),
-dataInicio varchar (12),
-acuraciaInicio varchar (2),
-dataFim varchar (12),
-acuraciaFim varchar (2));
+CREATE TABLE endereco (
+surrogateKey VARCHAR (10),
+tipo INT,
+endereco VARCHAR (40),
+bairro VARCHAR (30),
+distrito VARCHAR (30),
+municipio VARCHAR (30),
+estado VARCHAR (20),
+cep VARCHAR (10),
+caixaPostal VARCHAR (15),
+pais VARCHAR (30),
+dataInicio VARCHAR (12),
+acuraciaInicio VARCHAR (2),
+dataFim VARCHAR (12),
+acuraciaFim VARCHAR (2));
 
 /*Criando tabela de vínculo*/
-create table vinculo ( 
-surrogateKey varchar (10),
-idCod varchar (10),
-nomePessoa varchar (40),
-relacionamento int,
-dataInicio varchar (12),
-dataFim varchar (12));
+CREATE TABLE vinculo ( 
+surrogateKeyPessoaVinculada VARCHAR (10),
+relacionamento INT,
+dataInicio VARCHAR (12),
+dataFim VARCHAR (12));
 
 /*Criando tabela de comunicação*/
-create table comunicacao (
-surrogateKey varchar (10),
-meio int,
-detalhe varchar (20),
-preferencia int,
-utilizacao int);
+CREATE TABLE comunicacao (
+surrogateKey VARCHAR (10),
+meio INT,
+detalhe VARCHAR (20),
+preferencia INT,
+utilizacao INT);
